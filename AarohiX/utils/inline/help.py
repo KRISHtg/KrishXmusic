c9241155f4d3922d0f4bac919488652886cbad51
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-05-22 17:02:32 +0530

    Update help.py

--- AarohiX/utils/inline/help.py
+++ AarohiX/utils/inline/help.py
@@ -11,7 +11,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 # creating first partition of menu
 
 def first_page(_):
-	controll_button = [InlineKeyboardButton(text="◁", callback_data=f"Adisa"), InlineKeyboardButton(text="HOME", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="▷", callback_data=f"dilXaditi")]
+	controll_button = [InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"Adisa"), InlineKeyboardButton(text="ʜᴏᴍᴇ", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="ɴᴇxᴛ", callback_data=f"dilXaditi")]
 	first_page_menu = InlineKeyboardMarkup(
 		[
 			[InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"), InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"),InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3")],
@@ -29,10 +29,10 @@ def first_page(_):
 # creating second partition of menu
 
 def second_page(_):
-	controll_button = [InlineKeyboardButton(text="◁", callback_data=f"settings_back_helper_fixed"), InlineKeyboardButton(text="HOME", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="▷", callback_data=f"settings_back_helper")]
+	controll_button = [InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settings_back_helper_fixed"), InlineKeyboardButton(text="ʜᴏᴍᴇ", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="ɴᴇxᴛ", callback_data=f"settings_back_helper")]
 	second_page_menu = InlineKeyboardMarkup(
 		[
-			[InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb16"), InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb17"),InlineKeyboardButton(text=_["H_B_18"], callback_data="help_callback hb18")],
+			[InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb16"), InlineKeyboardButton(text=_["H_B_17"], callback_data="help_callback hb17"),InlineKeyboardButton(text=_["H_B_18"], callback_data="help_callback hb18")],
                         [InlineKeyboardButton(text=_["H_B_19"], callback_data="help_callback hb19"), InlineKeyboardButton(text=_["H_B_20"], callback_data="help_callback hb20"),InlineKeyboardButton(text=_["H_B_21"], callback_data="help_callback hb21")],
                         [InlineKeyboardButton(text=_["H_B_22"], callback_data="help_callback hb22"), InlineKeyboardButton(text=_["H_B_23"], callback_data="help_callback hb23"),InlineKeyboardButton(text=_["H_B_24"], callback_data="help_callback hb24")],
 			[InlineKeyboardButton(text=_["H_B_25"], callback_data="help_callback hb25"), InlineKeyboardButton(text=_["H_B_26"], callback_data="help_callback hb26"),InlineKeyboardButton(text=_["H_B_27"], callback_data="help_callback hb27")],
