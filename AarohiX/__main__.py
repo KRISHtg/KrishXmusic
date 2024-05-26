e0c1040cd5e6d926d08012f2e1199d42556475d5
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-05-26 13:01:40 +0530

    Update __main__.py

--- AarohiX/__main__.py
+++ AarohiX/__main__.py
@@ -1,16 +1,18 @@
 import asyncio
 import importlib
-
+from sys import argv
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from AarohiX import LOGGER, app, userbot
-from AarohiX.core.call import Dil
+from AarohiX.core.call import VIP
 from AarohiX.misc import sudo
 from AarohiX.plugins import ALL_MODULES
 from AarohiX.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
+
+from VIPMUSIC.plugins.tools.clone import restart_bots
 
 
 async def init():
@@ -21,8 +23,10 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
+        LOGGER(__name__).error(
+            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧"
+        )
+
     await sudo()
     try:
         users = await get_gbanned()
@@ -30,33 +34,31 @@ async def init():
             BANNED_USERS.add(user_id)
         users = await get_banned_users()
         for user_id in users:
-            BANNED_USERS.add(user_id)
+            if user_id not in BANNED_USERS:
+                BANNED_USERS.add(user_id)
     except:
         pass
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("AarohiX.plugins" + all_module)
-    LOGGER("AarohiX.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴀʟʟ ᴍᴏᴅᴜʟᴇs...")
+    LOGGER("AarohiX.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    
     await userbot.start()
+
     await Dil.start()
-    try:
-        await Dil.stream_call("https://te.legra.ph/file/39b302c93da5c457a87e3.mp4")
-    except NoActiveGroupCall:
-        LOGGER("AarohiX").error(
-            "ʙsᴅᴋ ᴠᴄ ᴛᴏ ᴏɴ ᴋᴀʀʟᴇ  ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ ᴋɪ.\n\n ᴏɴ ᴋᴀʀᴋᴇ ᴀᴀ ᴛᴀʙ ᴛᴀᴋ ʙᴏᴛ ʙᴀɴᴅ ᴋᴀʀ ʀʜᴀ ʜᴏᴏɴ..."
-        )
-        exit()
-    except:
-        pass
     await Dil.decorators()
-    LOGGER("AarohiX").info(
-        "ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ, ɴᴏᴡ ɢɪʙ ʏᴏᴜʀ ɢɪʀʟғʀɪᴇɴᴅ ᴄʜᴜᴛ ɪɴ @LOVE_FEELINGS_WILL1"
-    )
+    await restart_bots()
+    LOGGER("AarohiX").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗞𝗥𝗜𝗦𝗛♨️\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
+
     await app.stop()
     await userbot.stop()
-    LOGGER("AarohiX").info("ᴍᴀᴀ ᴄʜᴜᴅᴀ ᴍᴀɪɴ ʙᴏᴛ ʙᴀɴᴅ ᴋᴀʀ ʀʜᴀ AᴀʀᴏʜɪX Mᴜsɪᴄ Bᴏᴛ...")
+
+    LOGGER("AarohiX").info(
+        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗞𝗥𝗜𝗦𝗛♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    )
 
 
 if __name__ == "__main__":
+
     asyncio.get_event_loop().run_until_complete(init())
