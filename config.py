2f35ed30d5ce984d8efbfdd0b69297b930dc2f9c
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-05-27 05:45:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+rCTz82Fv96NhMzc9")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+dr0Ygfku261jNGJl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/krishoffical2")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+rCTz82Fv96NhMzc9")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
