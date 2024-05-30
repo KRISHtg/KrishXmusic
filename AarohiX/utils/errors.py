991762c1b158099cb47a855aa7a41aa91aa96a83
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-05-31 04:14:00 +0530

    Update errors.py

--- AarohiX/utils/errors.py
+++ AarohiX/utils/errors.py
@@ -9,7 +9,7 @@ from pyrogram.types import CallbackQuery
 from AarohiX import app
 
 
-LOG_CHANNEL = ("LOG_CHANNEL", "https://t.me/alonegroup121")
+LOG_CHANNEL = ("LOG_CHANNEL", "https://t.me/queen_suportgroup")
 
 
 
