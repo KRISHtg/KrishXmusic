75fe1bbb152f8a98b0f780f4375bd61852182765
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-05-31 06:30:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KRISHtg/Musicbot",
+    "https://github.com/KRISHtg/queenmusicbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
