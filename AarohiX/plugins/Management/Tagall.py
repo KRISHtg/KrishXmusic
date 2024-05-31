16f4602ae6b9627d03f925114afe54825ebae67d
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-05-31 09:33:12 +0530

    Update Tagall.py

--- AarohiX/plugins/Management/Tagall.py
+++ AarohiX/plugins/Management/Tagall.py
@@ -6,53 +6,45 @@ from AarohiX.utils.admin_check import admin_filter
 
 
 
-SPAM_CHATS = []
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
 
 
 @app.on_message(filters.command(["utag", "all"]) & filters.group & admin_filter)
 async def tag_all_users(_,message): 
-    replied = message.reply_to_message  
-    if len(message.command) < 2 and not replied:
+    chat_id = message.chat.id
+    direp = message.reply_to_message
+    args = get_arg(message)
+    if not direp and not args:
         await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ**") 
         return                  
     if replied:
-        SPAM_CHATS.append(message.chat.id)      
-        usernum= 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id): 
-            if message.chat.id not in SPAM_CHATS:
-                break       
-            usernum += 5
-            usertxt += f" [{m.user.first_name}](tg://user?id={m.user.id})"
-            if usernum == 1:
-                await replied.reply_text(usertxt)
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try :
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-    else:
-        text = message.text.split(None, 1)[1]
-        
-        SPAM_CHATS.append(message.chat.id)
-        usernum= 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):       
-            if message.chat.id not in SPAM_CHATS:
-                break 
-            usernum += 1
-            usertxt += f" [{m.user.first_name}](tg://user?id={m.user.id})"
-            if usernum == 5:
-                await app.send_message(message.chat.id,f'{text}\n{usertxt}')
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""                          
-        try :
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass        
+        spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.get_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
+        if usrnum == 4:
+            if args:
+                txt = f"{args}\n\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            elif direp:
+                await direp.reply(usrtxt)
+            await sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
            
 @app.on_message(filters.command("cancel") & ~filters.private)
 async def cancelcmd(_, message):
