88e874bbcebfa7754f1559f7a89d29334fd3d57e
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 04:36:49 +0530

    Update mongo.py

--- AarohiX/utils/mongo.py
+++ AarohiX/utils/mongo.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.VIPMUSIC
+db = mongo.AarohiX
 
 coupledb = db.couple
 
