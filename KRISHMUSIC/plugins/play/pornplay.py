130cd03d864cbcf8fe12dc66bde4537a4fdc5447
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 12:40:01 +0530

    Update pornplay.py

--- KRISHMUSIC/plugins/play/pornplay.py
+++ KRISHMUSIC/plugins/play/pornplay.py
@@ -1,13 +1,13 @@
 from pyrogram import filters
 import requests, random
 from bs4 import BeautifulSoup
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 import pytgcalls
 import os, yt_dlp 
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pytgcalls.types import AudioVideoPiped
-from ANNIEMUSIC.plugins.play import play
-from ANNIEMUSIC.plugins.play.pornplay import play
+from KRISHMUSIC.plugins.play import play
+from KRISHMUSIC.plugins.play.pornplay import play
 
 #
 #####
