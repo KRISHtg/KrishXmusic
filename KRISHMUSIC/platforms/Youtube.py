71c4565857bbaf4989aeef42dc7906c9e6c590c1
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 11:41:00 +0530

    Update Youtube.py

--- KRISHMUSIC/platforms/Youtube.py
+++ KRISHMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ANNIEMUSIC.utils.database import is_on_off
-from ANNIEMUSIC.utils.formatters import time_to_seconds
+from KRISHMUSIC.utils.database import is_on_off
+from KRISHMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
