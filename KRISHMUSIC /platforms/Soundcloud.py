ebd2cde45ea50e806813be5762445e72ce874423
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 06:40:19 +0530

    Update Soundcloud.py

--- KRISHMUSIC /platforms/Soundcloud.py
+++ KRISHMUSIC /platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ANNIEMUSIC.utils.formatters import seconds_to_min
+from KRISHMUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
