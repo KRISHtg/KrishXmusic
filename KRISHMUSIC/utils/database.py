8ab6736cfd26e4acc2f5e0bb8da16dd42e722691
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:28:52 +0530

    Update database.py

--- KRISHMUSIC/utils/database.py
+++ KRISHMUSIC/utils/database.py
@@ -1,8 +1,8 @@
 import random
 from typing import Dict, List, Union
 
-from ANNIEMUSIC import userbot
-from ANNIEMUSIC.core.mongo import mongodb
+from KRISHMUSIC import userbot
+from KRISHMUSIC.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -67,7 +67,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from ANNIEMUSIC.core.userbot import assistants
+    from KRISHMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -81,7 +81,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from ANNIEMUSIC.core.userbot import assistants
+    from KRISHMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -108,7 +108,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from ANNIEMUSIC.core.userbot import assistants
+    from KRISHMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -121,7 +121,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from ANNIEMUSIC.core.userbot import assistants
+    from KRISHMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
