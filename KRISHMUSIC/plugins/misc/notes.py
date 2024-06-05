388a839327f67a836f0550e471c236750b22740d
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 12:33:29 +0530

    Update notes.py

--- KRISHMUSIC/plugins/misc/notes.py
+++ KRISHMUSIC/plugins/misc/notes.py
@@ -1,10 +1,10 @@
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 from config import BOT_USERNAME
 from pyrogram import filters
-from ANNIEMUSIC.utils.jarvis_ban import admin_filter
-from ANNIEMUSIC.mongo.notesdb import *
-from ANNIEMUSIC.utils.notes_func import GetNoteMessage, exceNoteMessageSender, privateNote_and_admin_checker
-from ANNIEMUSIC.utils.yumidb import user_admin
+from KRISHMUSIC.utils.jarvis_ban import admin_filter
+from KRISHMUSIC.mongo.notesdb import *
+from KRISHMUSIC.utils.notes_func import GetNoteMessage, exceNoteMessageSender, privateNote_and_admin_checker
+from KRISHMUSIC.utils.yumidb import user_admin
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , Message , CallbackQuery
 from pyrogram.enums import ChatMemberStatus
 
