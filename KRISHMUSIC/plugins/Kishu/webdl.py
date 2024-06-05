8e898733c0da77038f3c261549b58339d9d97d0d
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 11:51:11 +0530

    Update webdl.py

--- KRISHMUSIC/plugins/Kishu/webdl.py
+++ KRISHMUSIC/plugins/Kishu/webdl.py
@@ -9,7 +9,7 @@ import requests
 from pyrogram import Client, filters
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 
 def download_website(url):
