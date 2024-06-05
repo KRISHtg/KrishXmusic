8d216ae2d4da674d4e8d566bb804a03c5359d8ee
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 06:49:42 +0530

    Update population.py

--- KRISHMUSIC /plugins/Kishu/population.py
+++ KRISHMUSIC /plugins/Kishu/population.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 
 @app.on_message(filters.command("population"))
