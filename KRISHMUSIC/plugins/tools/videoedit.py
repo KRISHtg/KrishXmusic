edc9e009d4ce1d250401e1bbcfdf0f36edf99e0b
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:17:48 +0530

    Update videoedit.py

--- KRISHMUSIC/plugins/tools/videoedit.py
+++ KRISHMUSIC/plugins/tools/videoedit.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pydub import AudioSegment
 import speech_recognition as sr
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 # --------------------------------------
 
 def convert_video_to_text(video_path):
