9329501abf8023e19bcf67adcd316a386fcf58f6
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 07:48:37 +0530

    Update autoend.py

--- KRISHMUSIC /plugins/sudo/autoend.py
+++ KRISHMUSIC /plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ANNIEMUSIC import app
-from ANNIEMUSIC.misc import SUDOERS
-from ANNIEMUSIC.utils.database import autoend_off, autoend_on
+from KRISHMUSIC import app
+from KRISHMUSIC.misc import SUDOERS
+from KRISHMUSIC.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
