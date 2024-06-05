94c2e97dc5a8ab807da9e79ce02e0747dc0b6188
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:11:49 +0530

    Update search.py

--- KRISHMUSIC/plugins/tools/search.py
+++ KRISHMUSIC/plugins/tools/search.py
@@ -4,7 +4,7 @@ from search_engine_parser.core.engines.google import Search as GoogleSearch
 from search_engine_parser.core.engines.stackoverflow import \
     Search as StackSearch
 from search_engine_parser.core.exceptions import NoResultsFound, NoResultsOrTrafficError
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 from pyrogram import filters
 
 
