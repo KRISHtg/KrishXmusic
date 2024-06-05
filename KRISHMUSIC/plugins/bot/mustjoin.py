bedc9ebf97e27110273a95caa9fbd2d625c83334
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 12:23:22 +0530

    Update mustjoin.py

--- KRISHMUSIC/plugins/bot/mustjoin.py
+++ KRISHMUSIC/plugins/bot/mustjoin.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 #--------------------------
 
