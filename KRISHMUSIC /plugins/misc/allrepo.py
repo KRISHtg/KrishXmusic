3f3c8f68127b00ce7799713fa7292e71a394b9c7
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 07:22:43 +0530

    Update allrepo.py

--- KRISHMUSIC /plugins/misc/allrepo.py
+++ KRISHMUSIC /plugins/misc/allrepo.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 # Function to chunk the repository info into smaller parts
 def chunk_string(text, chunk_size):
