00451517222277d521c97b0718d0c417c706d5c5
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 12:20:20 +0530

    Update unmute.py

--- KRISHMUSIC/plugins/admins/unmute.py
+++ KRISHMUSIC/plugins/admins/unmute.py
@@ -1,7 +1,7 @@
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 from pyrogram import filters,enums
 from pyrogram.types import ChatPermissions 
-from ANNIEMUSIC.utils.jarvis_ban import admin_filter
+from KRISHMUSIC.utils.jarvis_ban import admin_filter
 
 @app.on_message(filters.command("unmuteall") & admin_filter)
 async def unmute_all(_,msg):
