4f5aec9afc9f2e3f8fddeeb382edea6e0521c737
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:07:04 +0530

    Update phone.py

--- KRISHMUSIC/plugins/tools/phone.py
+++ KRISHMUSIC/plugins/tools/phone.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 import requests
 import json
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 def send_message(message, text):
     message.reply_text(text)
