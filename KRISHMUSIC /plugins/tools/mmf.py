fe382ea3d719b8df8831b45fc22ca67da03a867e
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 08:11:49 +0530

    Update mmf.py

--- KRISHMUSIC /plugins/tools/mmf.py
+++ KRISHMUSIC /plugins/tools/mmf.py
@@ -3,7 +3,7 @@ import textwrap
 from PIL import Image, ImageDraw, ImageFont
 from pyrogram import filters
 from pyrogram.types import Message
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 @app.on_message(filters.command("mmf"))
 async def mmf(_, message: Message):
