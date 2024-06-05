4ae3de62c9ceb90f1956ed18a8db57df2531d288
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 07:51:58 +0530

    Update raid.py

--- KRISHMUSIC /plugins/sudo/raid.py
+++ KRISHMUSIC /plugins/sudo/raid.py
@@ -2,8 +2,8 @@ import pyrogram
 import time
 from pyrogram import filters
 from pyrogram import Client
-from ANNIEMUSIC import app
-from ANNIEMUSIC.misc import SUDOERS
+from KRISHMUSIC import app
+from KRISHMUSIC.misc import SUDOERS
 
 # Define the spam command handler
 @app.on_message(filters.command("raid", prefixes="+") & SUDOERS)
