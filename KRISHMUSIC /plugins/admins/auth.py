0b6785067414628dc58805d3833e9dfb76a5194d
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 06:54:51 +0530

    Update auth.py

--- KRISHMUSIC /plugins/admins/auth.py
+++ KRISHMUSIC /plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ANNIEMUSIC import app
-from ANNIEMUSIC.utils import extract_user, int_to_alpha
-from ANNIEMUSIC.utils.database import (
+from KRISHMUSIC import app
+from KRISHMUSIC.utils import extract_user, int_to_alpha
+from KRISHMUSIC.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from ANNIEMUSIC.utils.decorators import AdminActual, language
-from ANNIEMUSIC.utils.inline import close_markup
+from KRISHMUSIC.utils.decorators import AdminActual, language
+from KRISHMUSIC.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
