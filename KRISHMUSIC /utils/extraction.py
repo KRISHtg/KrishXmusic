4df661a868cb47779b916394cb3fcfff1eba6175
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 08:48:17 +0530

    Update extraction.py

--- KRISHMUSIC /utils/extraction.py
+++ KRISHMUSIC /utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from ANNIEMUSIC import app
+from KRISHMUSIC import app
 
 
 async def extract_user(m: Message) -> User:
