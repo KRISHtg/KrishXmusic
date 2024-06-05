fda35d0a577f3bfab929e3ca86ff49f320d786bb
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:21:40 +0530

    Update admins.py

--- KRISHMUSIC/utils/decorators/admins.py
+++ KRISHMUSIC/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from ANNIEMUSIC import app
-from ANNIEMUSIC.misc import SUDOERS, db
-from ANNIEMUSIC.utils.database import (
+from KRISHMUSIC import app
+from KRISHMUSIC.misc import SUDOERS, db
+from KRISHMUSIC.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
