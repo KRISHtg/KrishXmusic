99a73cdbe9980c91188fb8c0c5eb7086e0e9e3e3
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:33:01 +0530

    Update mongo.py

--- KRISHMUSIC/utils/mongo.py
+++ KRISHMUSIC/utils/mongo.py
@@ -21,7 +21,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.ANNIEMUSIC
+db = mongo.KRISHMUSIC
 
 coupledb = db.couple
 
