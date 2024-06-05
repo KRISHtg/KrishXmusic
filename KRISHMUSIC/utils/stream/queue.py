8a63a48f5df184065c4b368c046e58c05285ae8e
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-05 13:25:28 +0530

    Update queue.py

--- KRISHMUSIC/utils/stream/queue.py
+++ KRISHMUSIC/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from ANNIEMUSIC.misc import db
-from ANNIEMUSIC.utils.formatters import check_duration, seconds_to_min
+from KRISHMUSIC.misc import db
+from KRISHMUSIC.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
