0f039a2aa9a59eb20d2380a43e458b56a643fde3
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-06 13:27:17 +0530

    Update repo.py

--- ANNIEMUSIC/plugins/bot/repo.py
+++ ANNIEMUSIC/plugins/bot/repo.py
@@ -31,13 +31,13 @@ async def start(_, msg):
             ],
      
             [
-             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/JARVIS_V2"),
-             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/JARVIS_V_SUPPORT"),
+             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/queen_suportgroup"),
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/Queen_update"),
              ],
      
              [
-             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/Dora_Hub"),          
-             InlineKeyboardButton("︎ᴍᴜsɪᴄ", url=f"https://github.com/doraemon890/ANNIE-X-MUSIC"),
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/queen_suportgroup"),          
+             InlineKeyboardButton("︎ᴍᴜsɪᴄ", url=f"https://github.com/KRISHtg/queenmusicbot"),
              ],
      
               ]
@@ -45,7 +45,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://telegra.ph/file/58afe55fee5ae99d6901b.jpg",
+        photo="https://telegra.ph/file/9e6ad9d95b0a61baf5dd1.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
