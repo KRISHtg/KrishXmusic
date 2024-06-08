6eaf21cd9c14507491d4d9c8fd4c2059710ee9f2
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-08 17:50:10 +0530

    Update bot.py

--- AarohiX/core/bot.py
+++ AarohiX/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class Dil(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Annie is on the way...")
         super().__init__(
             name="AarohiX",
             api_id=config.API_ID,
@@ -34,19 +34,19 @@ class Dil(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-            exit()
+            
         except Exception as ex:
             LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
-            exit()
+            
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
-            exit()
+            
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
