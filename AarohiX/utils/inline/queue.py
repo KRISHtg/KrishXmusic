8d4647c318b94183fb0b799538f75b93eaefc1d0
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-06-11 14:56:37 +0530

    Update queue.py

--- AarohiX/utils/inline/queue.py
+++ AarohiX/utils/inline/queue.py
@@ -67,10 +67,14 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="◁◁", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(
+                text="ᴏᴡɴᴇʀ",
+                url=f"https://t.me/ruhi_queen1",
+            ),
+            InlineKeyboardButton(
+                text="sᴜᴘᴘᴏʀᴛ",
+                url=f"https://t.me/+JH5lJV_0oVc0NjBl",
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
