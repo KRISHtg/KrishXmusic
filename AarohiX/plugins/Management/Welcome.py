f2d3afca9a99f7770e46ef2fe785055d4028980f
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-07-03 09:15:30 +0530

    Update Welcome.py

--- AarohiX/plugins/Management/Welcome.py
+++ AarohiX/plugins/Management/Welcome.py
@@ -137,7 +137,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ┣ 𝟓 ➥ 𝐆ɪᴠᴇ  ʀᴇsᴘᴇᴄᴛ , ᴛᴀᴋᴇ  ʀᴇsᴘᴇᴄᴛ 
 ┗━━━━━━━━━━━━━━━━━      
 
-❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [𝐐ᴜᴇᴇɴ ✘ 𝐌ᴜꜱɪᴄ](https://t.me/Queen_of_heart_music_bot)
+❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ 𝐐ᴜᴇᴇɴ ✘ 𝐌ᴜꜱɪᴄ
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/Queen_of_heart_music_bot?startgroup=true")]])
         )
