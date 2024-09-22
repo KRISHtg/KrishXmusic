2c56be061392963419897a52b0e1a12ca5b880d2
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-22 12:16:09 +0530

    Update thumbnails.py

--- AarohiX/utils/thumbnails.py
+++ AarohiX/utils/thumbnails.py
@@ -107,9 +107,9 @@ async def get_thumb(videoid):
     enhancer = ImageEnhance.Brightness(background)
     background = enhancer.enhance(0.6)
     draw = ImageDraw.Draw(background)
-    arial = ImageFont.truetype("AarohiX/assets/assets/font2.ttf", 30)
-    font = ImageFont.truetype("AarohiX/assets/assets/font.ttf", 30)
-    title_font = ImageFont.truetype("AarohiX/assets/assets/font3.ttf", 45)
+    arial = ImageFont.truetype("AarohiX/assets/font2.ttf", 30)
+    font = ImageFont.truetype("AarohiX/assets/font.ttf", 30)
+    title_font = ImageFont.truetype("AarohiX/assets/font3.ttf", 45)
 
 
     circle_thumbnail = crop_center_circle(youtube, 400, 20)
@@ -149,7 +149,7 @@ async def get_thumb(videoid):
     draw.text((text_x_position, 400), "00:00", (255, 255, 255), font=arial)
     draw.text((1080, 400), duration, (255, 255, 255), font=arial)
 
-    play_icons = Image.open("AarohiX/assets/assets/play_icons.png")
+    play_icons = Image.open("AarohiX/assets/play_icons.png")
     play_icons = play_icons.resize((580, 62))
     background.paste(play_icons, (text_x_position, 450), play_icons)
 
