7a9510d3a221e132114f33ce93bc92f639a8d1da
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-25 04:47:30 +0530

    Fix

--- config.py
+++ config.py
@@ -99,7 +99,6 @@ STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
-
 BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
