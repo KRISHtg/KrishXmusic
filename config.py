d0a7a9441e616bb3dc435388fcb540d98618ef7b
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-24 17:11:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,6 @@ UPSTREAM_REPO = getenv(
 )
 REPO = getenv("REPO", "https://github.com/KRISHtg/AarohiX")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-)  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Krrishofficial122")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+16FWxG30PUxhNzQ1")
