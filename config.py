0176d0b2da3d68e05c98dc17ec6f01243285c0d3
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-24 07:30:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,64 +6,62 @@ from pyrogram import filters
 
 load_dotenv()
 
+# Get this value from my.telegram.org/apps
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-API_ID = int(getenv("API_ID", ""))
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
 
-API_HASH = getenv("API_HASH", "")
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+# Chat id of a group for logging bot's activities
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 54000))
+# Get this value from @MissRose_Bot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", None))
 
-SONG_DOWNLOAD_DURATION = int(
-    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "54000")
-)
-
-LOGGER_ID = int(getenv("LOGGER_ID", -1001921995918))
-
-OWNER_ID = int(getenv("OWNER_ID", "6745609407"))
-
-BOT_USERNAME = getenv("BOT_USERNAME" , "anonXmusicbot")
-
-COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
-
+## Fill these variables if you're deploying on heroku.
+# Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-
+# Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KRISHtg/AarohiX",
+    "https://github.com/CyberPixelPro/AviaxMusic",
 )
-
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+9U8Td9WZ2dk1ZGU1")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+Kus9t8btLPRhYzll")
+
+# Set this to True if you want the assistant to automatically leave chats after an interval
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+
+# make your bots privacy from telegra.ph and put your url here 
+PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-AviaxMusic-08-14")
 
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+Kus9t8btLPRhYzll")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Kus9t8btLPRhYzll")
-
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
-
+# Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
-
+# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
-CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "5"))
 
+# Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
-
-
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
+# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 # Get your pyrogram v2 session from @Shsusu_bot on Telegram
 STRING1 = getenv("STRING_SESSION", "")
