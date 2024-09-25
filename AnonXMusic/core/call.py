34ff4128fdf908f3823a6ad994fb43e14806a8bb
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-25 13:29:43 +0530

    Update call.py

--- AnonXMusic/core/call.py
+++ AnonXMusic/core/call.py
@@ -35,7 +35,7 @@ from AnonXMusic.utils.exceptions import AssistantErr
 from AnonXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
 from AnonXMusic.utils.inline.play import stream_markup
 from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
+from AnonXMusic.utils.thumbnails import gen_thumb
 from strings import get_string
 
 autoend = {}
