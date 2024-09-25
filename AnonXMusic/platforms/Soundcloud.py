6a422c577db281aea45bd2c448344ef40d202639
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-25 14:46:29 +0530

    Fix

--- AnonXMusic/platforms/Soundcloud.py
+++ AnonXMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from AviaxMusic.utils.formatters import seconds_to_min
+from AnonXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
