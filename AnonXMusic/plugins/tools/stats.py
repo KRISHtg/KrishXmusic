f6e935f1d3055b8a0401ddb5986990ae5f90d8cd
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-26 15:00:32 +0530

    Fix

--- AnonXMusic/plugins/tools/stats.py
+++ AnonXMusic/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from AviaxMusic import app
-from AviaxMusic.core.userbot import assistants
-from AviaxMusic.misc import SUDOERS, mongodb
-from AviaxMusic.plugins import ALL_MODULES
-from AviaxMusic.utils.database import get_served_chats, get_served_users, get_sudoers,is_autoend,is_autoleave
-from AviaxMusic.utils.decorators.language import language, languageCB
-from AviaxMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from AnonXMusic import app
+from AnonXMusic.core.userbot import assistants
+from AnonXMusic.misc import SUDOERS, mongodb
+from AnonXMusic.plugins import ALL_MODULES
+from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers,is_autoend,is_autoleave
+from AnonXMusic.utils.decorators.language import language, languageCB
+from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
