742090fdb9326554e14c213046d6eec7eedec9eb
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-26 14:59:14 +0530

    Add files via upload

--- AnonXMusic/plugins/tools/stats.py
+++ AnonXMusic/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from AnonXMusic import app
-from AnonXMusic.core.userbot import assistants
-from AnonXMusic.misc import SUDOERS, mongodb
-from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from AviaxMusic import app
+from AviaxMusic.core.userbot import assistants
+from AviaxMusic.misc import SUDOERS, mongodb
+from AviaxMusic.plugins import ALL_MODULES
+from AviaxMusic.utils.database import get_served_chats, get_served_users, get_sudoers,is_autoend,is_autoleave
+from AviaxMusic.utils.decorators.language import language, languageCB
+from AviaxMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
@@ -60,8 +60,9 @@ async def overall_stats(client, CallbackQuery, _):
         served_users,
         len(ALL_MODULES),
         len(SUDOERS),
-        config.AUTO_LEAVING_ASSISTANT,
+        await is_autoend(),
         config.DURATION_LIMIT_MIN,
+        await is_autoleave()  
     )
     med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
     try:
