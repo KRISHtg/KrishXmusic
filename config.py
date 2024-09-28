a4ec81d3c9441baa2badbb6f6db428880cf48e9a
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 20:26:00 +0530

    Fix

--- config.py
+++ config.py
@@ -94,7 +94,7 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/4f59fb748e1990acfa297.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/5gvrca.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/qt7y1v.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
