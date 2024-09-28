d4c611f4c28f02d0781aa6faa4bca0586be31d95
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 06:38:14 +0530

    Fix

--- KrishXmusic/platforms/Youtube.py
+++ KrishXmusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BrandrdXMusic.utils.database import is_on_off
-from BrandrdXMusic.utils.formatters import time_to_seconds
+from KrishXmusic.utils.database import is_on_off
+from KrishXmusic.utils.formatters import time_to_seconds
 
 
 
