ad79b57c4269b18ccf08c2da6ba80dcd4425f847
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 12:28:37 +0530

    Fix

--- KrishXmusic/utils/stream/queue.py
+++ KrishXmusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 from pyrogram import Client, client
-from BrandrdXMusic.misc import db
-from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min
+from KrishXmusic.misc import db
+from KrishXmusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
