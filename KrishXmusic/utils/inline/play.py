0d7b21936f53e3988e02f6273cab03024f39a523
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 12:25:39 +0530

    Fix

--- KrishXmusic/utils/inline/play.py
+++ KrishXmusic/utils/inline/play.py
@@ -2,8 +2,8 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from BrandrdXMusic.utils.formatters import time_to_seconds
-from BrandrdXMusic import app
+from KrishXmusic.utils.formatters import time_to_seconds
+from KrishXmusic import app
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
