eff6f3be3abb61b339e1a0f07daf97c5e0a37ece
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 06:37:42 +0530

    Fix

--- KrishXmusic/platforms/Telegram.py
+++ KrishXmusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.formatters import (
+from KrishXmusic import app
+from KrishXmusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
