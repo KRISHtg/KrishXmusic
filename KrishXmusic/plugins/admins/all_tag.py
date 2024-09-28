303332cb02e7e66894d214ce71e5a8210434dacf
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 06:39:37 +0530

    Fix

--- KrishXmusic/plugins/admins/all_tag.py
+++ KrishXmusic/plugins/admins/all_tag.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters
 from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.branded_ban import admin_filter
+from KrishXmusic import app
+from KrishXmusic.utils.branded_ban import admin_filter
 
 
 SPAM_CHATS = []
