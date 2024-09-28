71b06e34c2ba48213c84cc3d949ccbfbe7f0ae2b
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 07:56:57 +0530

    Fix

--- KrishXmusic/plugins/tools/zowner.py
+++ KrishXmusic/plugins/tools/zowner.py
@@ -3,9 +3,9 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.mongo.afkdb import LOGGERS as OWNERS
-from BrandrdXMusic.utils.database import add_served_chat, get_assistant
+from KrishXmusic import app
+from KrishXmusic.mongo.afkdb import LOGGERS as OWNERS
+from KrishXmusic.utils.database import add_served_chat, get_assistant
 
 
 @app.on_message(filters.command("repo"))
