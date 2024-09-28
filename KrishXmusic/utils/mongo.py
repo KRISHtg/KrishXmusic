f27bb6c83869e76f537d090db810fcb821298576
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 13:58:00 +0530

    Fix

--- KrishXmusic/utils/mongo.py
+++ KrishXmusic/utils/mongo.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.BrandrdXMusic
+db = mongo.KrishXmusic
 
 coupledb = db.couple
 
