5092fdd02a50cd88cf79dc3edd6f4df4133ee808
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 08:07:55 +0530

    Fix

--- KrishXmusic/utils/decorators/admins.py
+++ KrishXmusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from BrandrdXMusic import app
-from BrandrdXMusic.misc import SUDOERS, db
-from BrandrdXMusic.utils.database import (
+from KrishXmusic import app
+from KrishXmusic.misc import SUDOERS, db
+from KrishXmusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
