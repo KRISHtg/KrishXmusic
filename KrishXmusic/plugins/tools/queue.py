9c3f5398f909f914d754ac077421260920ec2977
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 07:52:21 +0530

    Fix

--- KrishXmusic/plugins/tools/queue.py
+++ KrishXmusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from BrandrdXMusic import app
-from BrandrdXMusic.misc import db
-from BrandrdXMusic.utils import HottyBin, get_channeplayCB, seconds_to_min
-from BrandrdXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from BrandrdXMusic.utils.decorators.language import language, languageCB
-from BrandrdXMusic.utils.inline import queue_back_markup, queue_markup
+from KrishXmusic import app
+from KrishXmusic.misc import db
+from KrishXmusic.utils import HottyBin, get_channeplayCB, seconds_to_min
+from KrishXmusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from KrishXmusic.utils.decorators.language import language, languageCB
+from KrishXmusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
