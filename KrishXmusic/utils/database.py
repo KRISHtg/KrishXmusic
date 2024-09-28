066e95a64a70a88725ea20ae1d5ad6baa7e13c29
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 12:35:24 +0530

    Fix

--- KrishXmusic/utils/database.py
+++ KrishXmusic/utils/database.py
@@ -1,8 +1,8 @@
 import random
 from typing import Dict, List, Union
 
-from BrandrdXMusic import userbot
-from BrandrdXMusic.core.mongo import mongodb, pymongodb
+from KrishXmusic import userbot
+from KrishXmusic.core.mongo import mongodb, pymongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -191,7 +191,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from BrandrdXMusic.core.userbot import assistants
+    from KrishXmusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -205,7 +205,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from BrandrdXMusic.core.userbot import assistants
+    from KrishXmusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -232,7 +232,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from BrandrdXMusic.core.userbot import assistants
+    from KrishXmusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -245,7 +245,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from BrandrdXMusic.core.userbot import assistants
+    from KrishXmusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
