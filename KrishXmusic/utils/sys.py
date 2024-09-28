9b8fb8cac237a8a60d2fc2ffd5f558d2408c2361
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 12:42:54 +0530

    Fix

--- KrishXmusic/utils/sys.py
+++ KrishXmusic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from BrandrdXMusic.misc import _boot_
-from BrandrdXMusic.utils.formatters import get_readable_time
+from KrishXmusic.misc import _boot_
+from KrishXmusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
