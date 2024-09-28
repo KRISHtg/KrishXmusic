79c9018c8372b0348546f4cca9330eab61d23fc5
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 07:54:44 +0530

    Fix

--- KrishXmusic/plugins/tools/telegraph.py
+++ KrishXmusic/plugins/tools/telegraph.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from BrandrdXMusic import app
+from KrishXmusic import app
 import requests
 
 
