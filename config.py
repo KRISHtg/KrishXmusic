ee0abde994171cceab597307c677b174fcc0655d
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 13:45:20 +0530

    Fix

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WCGKING/BrandrdXMusic",
+    "https://github.com/KRISHtg/AnonXMusic2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
