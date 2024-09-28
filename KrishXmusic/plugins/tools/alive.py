e8c364b806b6717965de0b18a91ea82cd53a8918
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 07:43:39 +0530

    Fix

--- KrishXmusic/plugins/tools/alive.py
+++ KrishXmusic/plugins/tools/alive.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from BrandrdXMusic import app
+from KrishXmusic import app
 from pyrogram import filters
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
