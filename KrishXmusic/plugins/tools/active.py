38a538c07dfe5e897c1a83ab2e48fada70e3cfdf
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 07:42:53 +0530

    Fix

--- KrishXmusic/plugins/tools/active.py
+++ KrishXmusic/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
 
-from BrandrdXMusic import app
+from KrishXmusic import app
 from config import OWNER_ID
-from BrandrdXMusic.utils.database import (
+from KrishXmusic.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
