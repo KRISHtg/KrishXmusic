7ea3dc8cde927d492dc1021441656a4c8b5f4bb3
KRISH <168031558+KRISHtg@users.noreply.github.com>
2024-09-28 12:38:19 +0530

    Fix

--- KrishXmusic/utils/extraction.py
+++ KrishXmusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from BrandrdXMusic import app
+from KrishXmusic import app
 
 
 async def extract_user(m: Message) -> User:
